1300f8ab9db580bbc04aa5b7e0b581ff840d7147
StartApp-SDK <yaniv.avraham@startapp.com>
2018-03-07 15:48:02 +0200

    Updated to SDK 3.6.0. New StartApp icons.

--- iosSwiftExampleApp/StartApp.framework/Versions/A/Headers/STAStartAppAd.h
+++ iosSwiftExampleApp/StartApp.framework/Versions/A/Headers/STAStartAppAd.h
@@ -3,25 +3,15 @@
 //  StartAppAdSDK
 //
 //  Copyright (c) 2013 StartApp. All rights reserved.
-//  SDK version 3.4.3
+//  SDK version 3.6.0
 
 #import <UIKit/UIKit.h>
 #import "STAAbstractAd.h"
-
-/* StartAppAd enumeration */
-typedef enum {
-	STAAdType_FullScreen = 1,
-    STAAdType_Automatic = 3,
-    STAAdType_Overlay = 5
-} STAAdType;
-
+#import "STAAdPreferences.h"
 
 @interface STAStartAppAd : STAAbstractAd
 
 @property (nonatomic, assign) bool STAShouldAutoRotate;
-@property STAAdType startAppAdType;
-
-@property (nonatomic, strong) STAAdPreferences *preferences;
 
 - (id) init;
 
@@ -37,14 +27,7 @@ typedef enum {
 - (void) showAd;
 - (void) showAdWithAdTag:(NSString *)adTag;
 
-// STAAdType is depreceted, will be removed in future SDKs.
-- (void) loadAd:(STAAdType) adType __attribute__((deprecated));
-- (void) loadAd:(STAAdType) adType withAdPreferences:(STAAdPreferences*) adPrefs __attribute__((deprecated));
-- (void) loadAd:(STAAdType) adType withDelegate:(id<STADelegateProtocol>) delegate __attribute__((deprecated));
-- (void) loadAd:(STAAdType) adType withDelegate:(id<STADelegateProtocol>) delegate withAdPreferences:(STAAdPreferences*) adPrefs __attribute__((deprecated));
-
-// initWithAppID is depreceted, will be removed in future SDKs, please use StartAppSDK instead.
-+ (void) initWithAppId: (NSString*)applicationId developerId:(NSString*)developerId __attribute__((deprecated));
+- (void) closeAd;
 
 
 - (BOOL) isReady;
